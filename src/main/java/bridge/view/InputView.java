d9960df3ec4753ebe334529877d4e02345dfd99e
Choo <choo000407@naver.com>
2023-12-13 16:19:30 +0900

    feat(InputView): 입력 안내 문구를 함께 출력한다

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
         String inputBridgeSize = Console.readLine();
         return parseInputBridgeSizeToInt(inputBridgeSize);
     }
@@ -19,11 +20,13 @@ public class InputView {
     }
 
     public MoveDirection readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
         String inputMoving = Console.readLine();
         return MoveDirection.findByName(inputMoving);
     }
 
     public GameCommand readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         String inputGameCommand = Console.readLine();
         return GameCommand.findByName(inputGameCommand);
     }
